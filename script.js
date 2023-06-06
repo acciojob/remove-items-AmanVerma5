18431e15318601f054d067c14aed125a39698e9b
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-06 08:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const colorSelect = document.getElementById('colorSelect');
+const removeButton = document.getElementById('removeButton');
+
+removeButton.addEventListener('click', function() {
+  const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+  colorSelect.removeChild(selectedOption);
+});
